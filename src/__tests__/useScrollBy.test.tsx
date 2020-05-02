ce57cdbdebe34077fea3d11573fb9ad2745ebc5f
Dev Boldly <devboldly@gmail.com>
2020-05-02 15:57:26 -0500

    Update documentation

--- src/__tests__/useScrollBy.test.tsx
+++ src/__tests__/useScrollBy.test.tsx
@@ -1,9 +1,6 @@
 import { renderHook } from '@testing-library/react-hooks';
 import { useScrollBy } from '../hooks/useScrollBy';
 
-// Learn how to test React hooks:
-// https://react-hooks-testing-library.com/
-
 describe('useScrollBy Hook', () => {
   test('should run without crashing', () => {
     const { result } = renderHook(() => useScrollBy());
--- src/__tests__/useScrollTo.test.tsx
+++ src/__tests__/useScrollTo.test.tsx
@@ -1,9 +1,6 @@
 import { renderHook } from '@testing-library/react-hooks';
 import { useScrollTo } from '../hooks/useScrollTo';
 
-// Learn how to test React hooks:
-// https://react-hooks-testing-library.com/
-
 describe('useScrollTo Hook', () => {
   test('should run without crashing', () => {
     const { result } = renderHook(() => useScrollTo());
