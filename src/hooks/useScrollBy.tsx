c234a138f530ccc117a3f57844295e02e94f8a1c
Dev Boldly <devboldly@gmail.com>
2020-05-02 20:33:32 -0500

    Update docs

--- src/hooks/useScrollBy.tsx
+++ src/hooks/useScrollBy.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import smoothscroll from 'smoothscroll-polyfill';
 
 /**
- * Documentation: https://devboldly.github.io/react-use-window-scroll/useScrollBy
+ * See documentation: [useScrollBy](https://devboldly.github.io/react-use-window-scroll/useScrollBy)
  *
  * This hook scrolls the page by the specified vertical and horizontal amount by using the Window Web API's Window.scrollBy function.
  * Smooth scrolling behavior (native to the browser) is supported by providing ScrollToOptions.
