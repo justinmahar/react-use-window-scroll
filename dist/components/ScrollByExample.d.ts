82083fa462167fc896ad8c08f3485d112cd49727
Justin Mahar <contact@justinmahar.com>
2024-02-09 16:52:03 -0500

    Run updater; update stories

--- dist/components/ScrollByExample.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-export interface ScrollByExampleProps {
-}
-export declare function ScrollByExample(props: ScrollByExampleProps): JSX.Element;
--- dist/components/ScrollToExample.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-export interface ScrollToExampleProps {
-}
-export declare function ScrollToExample(props: ScrollToExampleProps): JSX.Element;
